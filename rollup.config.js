255290835a4a55e092dc961fc2319b18a472eb74
pengtikui <949828390@qq.com>
2019-04-06 00:22:52 +0800

    :wrench: 更新 rollup 配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+
 export default {
   input: './src/index.js',
   output: {
     file: './dist/mp-redux.js',
     format: 'cjs',
   },
+  plugins: [
+    nodeResolve({
+      jsnext: true
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+  ],
 };
